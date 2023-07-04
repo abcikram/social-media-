a2b0f338040f8135fa6acb2d0b03d121ba44ed45
abcikram <110589147+abcikram@users.noreply.github.com>
2023-07-04 23:28:36 +0530

    hello hi

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use('/api/chat',chatRouter)
 app.use('/api/message',messageRouter)
 
 
-const PORT =  process.env.PORT || 8002;
+const PORT =  process.env.PORT || 8000;
 
 app.listen(PORT,() =>{
     console.log(`Server running on port ${PORT}`.yellow.bold)
